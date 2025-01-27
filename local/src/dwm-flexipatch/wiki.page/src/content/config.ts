64fd0b2300d0b1c8490073ecbfcc3a3b59bac9ca
Ahmad Jerjawi <72747814+ahmadjerjawi@users.noreply.github.com>
2025-01-27 09:56:43 +0000

    Delete local directory

--- local/src/dwm-flexipatch/wiki.page/src/content/config.ts
+++
@@ -1,7 +0,0 @@
-import { defineCollection } from 'astro:content';
-import { docsSchema } from '@astrojs/starlight/schema';
-import { Steps } from '@astrojs/starlight/components';
-
-export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
-};
